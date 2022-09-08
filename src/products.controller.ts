ecdfd395054c0e85a869bd7c89a765d875790599
KauanAmorim <kauanamorim88@gmail.com>
2022-09-08 14:03:53 -0300

    firsts steps on creating controller and his methods

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -1,9 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get()
   getAll(): string {
     return 'All products';
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return `return products with id: ${id}`;
+  }
+
+  @Post()
+  create(@Body() product): string {
+    console.log(product);
+    return 'Product created';
+  }
+
+  @Put()
+  update(@Body() product): string {
+    console.log(product);
+    return 'Product updated';
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `product ${id} deleted`;
   }
 }
